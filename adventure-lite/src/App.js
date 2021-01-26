58a916b983ac50b80d41f57b220dd5b378d3bc15
Forsteee <55308823+Forsteee@users.noreply.github.com>
2021-01-27 03:26:03 +0500

    bd_complite

--- adventure-lite/src/App.js
+++ adventure-lite/src/App.js
@@ -4,12 +4,18 @@ import green from './green.png';
 import white from './white.png';
 import grey from './grey.png';
 import './App.css';
+
+//const dbConnection = require("./models/dbConnection");
+//const init_models = require('./models/init-models');
+
 const {useState} = require("react");
 const  useInterval = require("./useInterval");
 
 const axios = require('axios');
 
-axios.get('http://localhost:3001/landscape')
+function testbd(){// переделать под игрульку
+
+axios.get('http://localhost:3001/landscape')// пример получения
     .then(function (response) {
       // handle success
       console.log(response);
@@ -18,9 +24,19 @@ axios.get('http://localhost:3001/landscape')
       // handle error
       console.log(error);
     })
+    axios.post(`http://localhost:3001/leaderbord/add`,{// пример добавления
+        Name: '',
+        Time: '',
+        Score: '',
+        }
+    );
+}
 
-const FIELD_SIZE = 16;
+const FIELD_SIZE = 17;
 const FIELD_ROW = [...new Array(FIELD_SIZE).keys()];
+
+const textureMap1x = [1,2,3,4,5,6,7,8,9];
+const textureMap1y = [1,1,1,1,2,2,2,3,4];
 
 
 let randItem = {
@@ -35,9 +51,15 @@ const  DIRECTION = {
   40: {x: 0, y: 1},
 }
 
-function getItem(x,y,personSegments) {
+function getItem(x,y,personSegments, x1, y1) {
   if(randItem.x === x && randItem.y === y){
     return green
+  }
+
+  for(let i = 0; i< x1.length; i++){
+    if(x1[i] === x && y1[i] === y){
+      return white
+    }
   }
 
   for (const  segment of personSegments){
@@ -51,32 +73,43 @@ function limitByField(j) {
   if(j >= FIELD_SIZE){
     return 0;
   }
-  if(j < 0){
-    return  FIELD_SIZE - 1;
+  if(j < 0) {
+    return FIELD_SIZE - 1;
   }
  return j;
 }
 
-function newPersonPosition(segments, direction) {
-  return segments.map(segment => ({
-    x:limitByField(segment.x + direction.x),
-    y:limitByField(segment.y + direction.y)
-  }));
+function newPersonPosition(segments, direction,xMap,yMap) {
+  let go = true;
+  let x1 = segments[0].x + direction.x;
+  let y1 = segments[0].y + direction.y;
+  for(let i = 0; i< xMap.length; i++) {
+    if (x1 == xMap[i] && y1 == yMap[i]) {
+      go = false
+    }
+  }
+  if(go){
+    return segments.map(segment => ({
+      x: limitByField(segment.x + direction.x),
+      y: limitByField(segment.y + direction.y)
+    }));
+    }
+  else {
+    return segments.map(segment => ({
+      x: limitByField(segment.x),
+      y: limitByField(segment.y)
+    }));
+  }
+
 }
 
 function App() {
-
-
-
   const [personSegments,setPersonSegments] = useState([
     {x:8,y:8}
   ]);
 
-
-  const [direction, setDirection] = useState(DIRECTION["37"]);
-
   const movePerson = ({keyCode}) =>
-      keyCode >= 37 && keyCode <= 40 && setPersonSegments(segments => newPersonPosition(segments, DIRECTION[keyCode]));
+      keyCode >= 37 && keyCode <= 40 && setPersonSegments(segments => newPersonPosition(segments, DIRECTION[keyCode], textureMap1x,textureMap1y));
 
   return (
 <div role="button" tabIndex="0" onKeyDown={e => movePerson(e)} >
@@ -86,7 +119,7 @@ function App() {
         {FIELD_ROW.map(y =>(
             <div class="sqrow" key={y}>
               {FIELD_ROW.map(x => (
-                  <img class="sq" key={x} src={getItem(x,y,personSegments) || grey}/>
+                  <img class="sq" key={x} src={getItem(x,y,personSegments, textureMap1x ,textureMap1y) || grey}/>
               ))}
             </div>
         ))}
